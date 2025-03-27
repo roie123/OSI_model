82348d0b35ec6a6333c5e381ff9e79ec20895557
roie123 <roieivri123@gmail.com>
2025-03-27 14:58:42 +0200

    added known devices feature for the network_layer

---
+++ networking_project/known_devices.h
@@ -0,0 +1,21 @@
+//
+// Created by royivri on 3/27/25.
+//
+
+#ifndef KNOWN_DEVICES_H
+#define KNOWN_DEVICES_H
+
+extern typedef struct known_devices_list {
+    char first[50];
+    char second[50];
+    char third[50];
+    char fourth[50];
+    char fifth[50];
+
+} known_devices_list;
+
+
+
+
+
+#endif //KNOWN_DEVICES_H
