16e4c88a62c0b80a3f39f95f73ead0217423379d
roie123 <roieivri123@gmail.com>
2025-03-28 18:35:53 +0300

    made the output nicer

--- networking_project/data_link.c
+++ networking_project/data_link.c
@@ -1,4 +1,6 @@
 #include "data_link.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -13,6 +15,9 @@ Wrapped_Request add_mac_address(Wrapped_Request *request) {
 strcpy(request->device[0].MAC_address,get_device_info_A().MAC_address);
 strcpy(request->device[1].MAC_address,get_info_device_B().MAC_address);
 
+    printf("\n <======================= DATA  LAYER =======================> \n");
+    printf("Adding MAC address:%s to %s\n",request->device[0].MAC_address,request->device[0].device_name);
+    printf("Adding MAC address:%s to %s\n",request->device[1].MAC_address,request->device[1].device_name);
 
 return *request;
 }
--- networking_project/main.c
+++ networking_project/main.c
@@ -15,8 +15,6 @@ Segmented_data segmented_data =segment_data(con,e);
 Wrapped_Request request=wrapped_request_creation(segmented_data);
 request=add_mac_address(&request);
 
-    printf("%s\n",request.device[0].MAC_address);
-    printf("%s\n",request.device[1].MAC_address);
 
     return 0;
 }
